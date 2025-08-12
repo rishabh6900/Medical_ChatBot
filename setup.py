7c3740fc562ebd130d87f8093c62cfe6106768d7
Rishabh Prajapati <prajapatirishabh071@gmail.com>
2025-08-12 19:03:07 +0530

    add

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="Medical_Chatbot"
+    version="0.1.0",
+    author="Rishabh Prajapati"
+    author_email="prajapatirishabh071@gmail.com"
+    packages=find_packages(),
+    install_requires=[]
+)
